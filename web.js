5655a9fa48ad8764f18038ade339ceda4b2263b0
John Lindsey <john.m.lindsey@gmail.com>
2013-07-11 04:29:48 +0000

    Printed using index.html

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send(getFile('index.html');
+  response.send(Buffer.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function getFile(filename){
+  return Buffer.toString(fs.readFileSync(filename));
+}
